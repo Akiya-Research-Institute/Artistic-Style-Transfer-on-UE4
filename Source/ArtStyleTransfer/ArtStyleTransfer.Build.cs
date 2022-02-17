2ae07527f7bd811ec88240409553d4f34115cbf3
Kenji Asaba <kasa@akiya-souken.co.jp>
2022-02-17 14:47:03 +0900

    Add binary

--- Source/ArtStyleTransfer/ArtStyleTransfer.Build.cs
+++ Source/ArtStyleTransfer/ArtStyleTransfer.Build.cs
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright 2022 Akiya Research Institute, Inc. All Rights Reserved
 
 using UnrealBuildTool;
 
--- Source/ArtStyleTransfer/ArtStyleTransfer.cpp
+++ Source/ArtStyleTransfer/ArtStyleTransfer.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright 2022 Akiya Research Institute, Inc. All Rights Reserved
 
 #include "ArtStyleTransfer.h"
 #include "Modules/ModuleManager.h"
