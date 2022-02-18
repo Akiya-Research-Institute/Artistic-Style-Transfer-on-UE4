16255ed1ceb5621a4130ab02b4064485b660cab5
Kenji Asaba <kasa@akiya-souken.co.jp>
2022-02-18 10:28:55 +0900

    Changed to BP project

--- Source/ArtStyleTransfer/ArtStyleTransfer.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright 2022 Akiya Research Institute, Inc. All Rights Reserved
-
-#include "ArtStyleTransfer.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ArtStyleTransfer, "ArtStyleTransfer" );
--- Source/ArtStyleTransfer/ArtStyleTransfer.h
+++
@@ -1,6 +0,0 @@
-// Copyright 2022 Akiya Research Institute, Inc. All Rights Reserved
-
-#pragma once
-
-#include "CoreMinimal.h"
-
